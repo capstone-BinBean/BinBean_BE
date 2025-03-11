e1600e7d4382eaf18800d2af80dda4648826772b
unknown <kim0615157@gmail.com>
2025-03-11 20:57:07 +0900

    init

---
+++ src/main/java/binbean/binbean_BE/BinbeanBeApplication.java
@@ -0,0 +1,13 @@
+package binbean.binbean_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinbeanBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinbeanBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/binbean/binbean_BE/BinbeanBeApplicationTests.java
@@ -0,0 +1,13 @@
+package binbean.binbean_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinbeanBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
