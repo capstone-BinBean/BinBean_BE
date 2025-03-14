565615e00d39609b08102ea6652df08d6fb3f36c
songkyung <min.songkyung@gmail.com>
2025-03-14 18:44:32 +0900

    feat: Security 관련 기본 config, filter 설정

---
+++ src/main/java/binbean/binbean_BE/auth/JwtTokenProvider.java
@@ -0,0 +1,14 @@
+package binbean.binbean_BE.auth;
+
+import java.nio.charset.StandardCharsets;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenProvider {
+
+    public JwtTokenProvider() {
+
+    }
+}
---
+++ src/main/java/binbean/binbean_BE/auth/filter/JwtVerificationFilter.java
@@ -0,0 +1,24 @@
+package binbean.binbean_BE.auth.filter;
+
+import binbean.binbean_BE.auth.JwtTokenProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtVerificationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+    }
+}
