bb2b000320ac1b5e3bcdb5ced986405a2c4e919e
songkyung <min.songkyung@gmail.com>
2025-03-17 14:29:43 +0900

    feat: globalExceptionHandler & ErrorResponse 생성

---
+++ src/main/java/binbean/binbean_BE/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package binbean.binbean_BE.exception;
+
+import binbean.binbean_BE.global.ErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ClientErrorException.class)
+    public ResponseEntity<ErrorResponse> handleClientErrorException(ClientErrorException e) {
+        return new ResponseEntity<>(new ErrorResponse(e.getStatus(), e.getMessage()), e.getStatus());
+    }
+
+    /**
+    * 서버 내부 발생 에러 핸들링
+    * 내부 에러 전달하는 대신 스테이터스 코드만 보내주는 걸로 간소화
+    */
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleClientErrorException(RuntimeException e) {
+        return ResponseEntity.internalServerError().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleClientErrorException(Exception e) {
+        return ResponseEntity.internalServerError().build();
+    }
+}
