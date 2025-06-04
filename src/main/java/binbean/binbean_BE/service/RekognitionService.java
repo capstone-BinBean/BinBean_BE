88d10e0eaed358f165bfaa7231fdb9cbc2bed53a
songkyung <min.songkyung@gmail.com>
2025-06-04 20:10:25 +0900

    refactor: 이미지 리사이즈 처리

--- src/main/java/binbean/binbean_BE/service/RekognitionService.java
+++ src/main/java/binbean/binbean_BE/service/RekognitionService.java
@@ -1,5 +1,6 @@
 package binbean.binbean_BE.service;
 
+import binbean.binbean_BE.constants.Constants;
 import binbean.binbean_BE.constants.Constants.FixedValue;
 import binbean.binbean_BE.dto.CurrentSeats;
 import binbean.binbean_BE.dto.DetectedItem;
@@ -7,7 +8,10 @@ import binbean.binbean_BE.dto.FloorList;
 import binbean.binbean_BE.dto.Position;
 import binbean.binbean_BE.dto.response.FloorPlanResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Base64;
@@ -43,17 +47,19 @@ public class RekognitionService {
     }
 
     public FloorPlanResponse getCurrentOccupiedSeats(MultipartFile file, FloorList floorList, int floorNumber) throws IOException {
+        BufferedImage originalImage = ImageIO.read(file.getInputStream());
+        byte[] imageBytes = resizeImage(originalImage, FixedValue.CCTV_IMAGE_WIDTH);
+
         List<DetectedItem> people = getDetectedItems(file);
         List<Position> seatPositions = floorList.seatPosition();
         // Euclidean distance 위치값 셋
         Set<Position> occupiedSeatsByEuclid = new HashSet<>();
         // 점유된 좌석
         Optional<CurrentSeats> currOccupiedSeats = Optional.empty();
-
-        var peoplePositions = people.stream().map(DetectedItem::positions).toList();
-
         try {
-            String imageBytes = Base64.getEncoder().encodeToString(file.getBytes());
+            var peoplePositions = people.stream().map(DetectedItem::positions).toList();
+            // ImageBytes BASE64로 인코딩
+            String encodedImageBytes = Base64.getEncoder().encodeToString(imageBytes);
             // 검출된 사람 수
             int peopleCount = !people.isEmpty() ? people.getFirst().value() : 0;
             // 좌표 위치 JSON 문자열 변환
@@ -63,7 +69,7 @@ public class RekognitionService {
             String currentSeatsJson = objectMapper.writeValueAsString(CurrentSeats.create(List.of(Position.create(10, 10), Position.create(10, 10))));
 
             String prompt =
-                "이미지는 다음과 같습니다. " + imageBytes  + "\n" +
+                "이미지는 다음과 같습니다. " + encodedImageBytes  + "\n" +
                 "다음은 이미지에서 검출된 사람들의 위치입니다:\n" +
                     peoplePositionsJson + "\n\n" +
                     "이미지에서 검출된 사람들의 수는 " + peopleCount + "명입니다.\n" +
@@ -74,11 +80,11 @@ public class RekognitionService {
                     "2. 사진의 왜곡을 평면화 처리 등을 통해 보정한 뒤(Perspective Transform, 투시 변환), 도면 좌석 위치와 정렬해 주세요.\n" +
                     "3. 사진의 회전 정도도 고려해 주세요.\n" +
                     "4. 사람의 위치가 좌석과 충분히 가까운 경우, 해당 좌석에 앉아 있다고 판단해 주세요.\n\n" +
-                    "5. 결과는 사람이 앉아 있는 좌석만 포함하여 아래 형식의 JSON으로 반환해 주세요:\n" +
+                    "5. 결과는 사람이 앉아 있는 좌석만 포함하여 아래 형식의 JSON으로 반환해 주세요 (단일 JSON 객체로 응답해 주세요):\n" +
                     currentSeatsJson;
 
             // 점유된 좌석 위치 리스트 반환
-            var response = geminiService.askGeminiWithImage(prompt, imageBytes);
+            var response = geminiService.askGeminiWithImage(prompt, encodedImageBytes);
             log.info("gemini response: {}", response);
 
             currOccupiedSeats = parseJsonToCurrentSeats(response);
@@ -116,7 +122,6 @@ public class RekognitionService {
                 nearest = seat;
             }
         }
-
         // 동적 임계값 계산
         double threshold = getDynamicThreshold(seatPositions);
 
@@ -139,7 +144,6 @@ public class RekognitionService {
                 count++;
             }
         }
-
         return count == 0 ? 0.0 : (totalDistance/count);
     }
 
@@ -151,8 +155,11 @@ public class RekognitionService {
 
     private List<DetectedItem> getDetectedItems(MultipartFile file) throws IOException {
         try {
+            BufferedImage originalImage = ImageIO.read(file.getInputStream());
+            byte[] imageBytes = resizeImage(originalImage, FixedValue.CCTV_IMAGE_WIDTH);
+
             Image awsImage = Image.builder()
-                .bytes(SdkBytes.fromByteArray(file.getBytes()))
+                .bytes(SdkBytes.fromByteArray(imageBytes))
                 .build();
 
             DetectLabelsRequest request = DetectLabelsRequest.builder()
@@ -186,6 +193,7 @@ public class RekognitionService {
                         })
                         .toList();
 
+                    log.info("Detected Person Positions: {}", positions);
                     DetectedItem item = DetectedItem.create(
                         label.name(),
                         count,
@@ -217,5 +225,19 @@ public class RekognitionService {
             .replace("```json", "")
             .replace("```", "")
             .trim();
+    }
+
+    public byte[] resizeImage(BufferedImage originalImage, int targetWidth) throws IOException {
+        int targetHeight = (int) (originalImage.getHeight() * ((double) targetWidth / originalImage.getWidth()));
+        BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, originalImage.getType());
+        Graphics2D graphics2D = resizedImage.createGraphics();
+        graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        graphics2D.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
+        graphics2D.dispose();
+
+        ByteArrayOutputStream byteArr = new ByteArrayOutputStream();
+        ImageIO.write(resizedImage, FixedValue.IMAGE_FORMAT, byteArr);
+        // resized image bytes
+        return byteArr.toByteArray();
     }
 }
